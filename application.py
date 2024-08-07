4c207e9e21f8220fa27f2dbd5935ad85afa7fd82
Yohannan PC <yohannanpchacko0814@gmail.com>
2024-08-07 18:32:44 +0530

    first commit

---
+++ application.py
@@ -0,0 +1,37 @@
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from flask import Flask,request,jsonify,render_template
+application=Flask(__name__)
+app=application
+
+# import Ridge regressor and Standard scalar pickle
+ridge_model=pickle.load(open('models/ridge.pkl','rb'))
+standard_scalar=pickle.load(open('models/scalar.pkl','rb'))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_data():
+    if request.method=='POST':
+        Temperature=float(request.form.get('Temperature'))
+        RH=float(request.form.get('RH'))
+        Ws=float(request.form.get('Ws'))
+        Rain=float(request.form.get('Rain'))
+        FFMC=float(request.form.get('FFMC'))
+        DMC=float(request.form.get('DMC'))
+        ISI=float(request.form.get('ISI'))
+        Classes=float(request.form.get('Classes'))
+        Region=float(request.form.get('Region'))
+        new_scaled_data=standard_scalar.transform([[Temperature,RH,Ws,Rain,FFMC,DMC,ISI,Classes,Region]])
+        result=ridge_model.predict(new_scaled_data)
+        return render_template('home.html',results=result[0])
+    else:        
+        return render_template('home.html')
+
+
+if __name__=='__main__':
+    app.run(host="0.0.0.0",debug=True)
