953527bb0c25ab7868f77755ce522af8ae3df978
Yohannan PC <yohannanpchacko0814@gmail.com>
2024-08-08 07:51:11 +0530

    added about page

--- application.py
+++ application.py
@@ -32,6 +32,9 @@ def predict_data():
     else:        
         return render_template('home.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__=='__main__':
-    app.run(host="0.0.0.0",debug=True)
+    app.run(host="0.0.0.0")
